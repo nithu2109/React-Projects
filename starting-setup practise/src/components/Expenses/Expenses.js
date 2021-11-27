c9cbd61cd8c57e9b728001b8b2061ae129f21be1
nithu2109 <nithish.vagu219@gmail.com>
2021-11-27 19:50:43 +0530

    added module 3

---
+++ starting-setup practise/src/components/Expenses/Expenses.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ExpenseItem from "./ExpenseItem";
+import Card from '../UI/Card';
+import './Expenses.css';
+
+const Expenses = (props) => {
+    return (
+        <Card className="expenses">
+         <ExpenseItem 
+            date={props.items[0].date} 
+            amount={props.items[0].amount} 
+            title={props.items[0].title} />
+         <ExpenseItem 
+            date={props.items[1].date} 
+            amount={props.items[1].amount} 
+            title={props.items[1].title} />
+         <ExpenseItem 
+            date={props.items[2].date} 
+            amount={props.items[2].amount} 
+            title={props.items[2].title} />
+         <ExpenseItem 
+            date={props.items[3].date} 
+            amount={props.items[3].amount} 
+            title={props.items[3].title} />
+
+        </Card>
+    )
+}
+
+export default Expenses
